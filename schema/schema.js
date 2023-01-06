0b35421f042c5a636c17fa8755f03438041e709e
XsidX <developer.sid@outlook.com>
2023-01-06 15:48:59 +0300

    add the resolver method

--- schema/schema.js
+++ schema/schema.js
@@ -6,6 +6,17 @@ const {
   GraphQLInt,
 } = graphql;
 
+//dummy data
+
+const data = [
+  { id: '1', name: 'Latifah', age: 21 },
+  {id: '2', name: 'Sid', age: 22},
+  {id: '3', name: 'Idris', age: 23},
+  {id: '4', name: 'John', age: 23},
+  {id: '5', name: 'Lotus', age: 23},
+];
+
+
 // Create types
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,10 +39,12 @@ const RootQuery = new GraphQLObjectType({
 
       resolve(parent, args) {
         // Code to get data from db / other source
+
+      return data.find((user) => user.id === args.id);
       },
     },
   },
-}); 
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
