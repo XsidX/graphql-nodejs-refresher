39f97d57b9111642ea578b24cd24122ef506fc0f
XsidX <developer.sid@outlook.com>
2023-01-06 15:04:39 +0300

    install grapghql and express grapghql

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
 
 const app = express();
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+}));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
