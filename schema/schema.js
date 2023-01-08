06c69b001fd1514012cbd152007c535769bb20e5
XsidX <developer.sid@outlook.com>
2023-01-08 15:00:21 +0300

    Create hobby type

--- schema/schema.js
+++ schema/schema.js
@@ -3,17 +3,27 @@ const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
+  GraphQLID,
   GraphQLInt,
 } = graphql;
 
 //dummy data
 
 const data = [
-  { id: '1', name: 'Latifah', age: 21 },
-  {id: '2', name: 'Sid', age: 22},
-  {id: '3', name: 'Idris', age: 23},
-  {id: '4', name: 'John', age: 23},
-  {id: '5', name: 'Lotus', age: 23},
+  { id: '1', name: 'Latifah', age: 21, profession: "Rich" },
+  {id: '2', name: 'Sid', age: 22, profession: "SWE" },
+  {id: '3', name: 'Idris', age: 23, profession: "Teacher" },
+  {id: '4', name: 'John', age: 24, profession: "Doctor" },
+  {id: '5', name: 'Lotus', age: 25, profession: "Baker" },
+
+];
+
+const hobbiesData = [
+  { id: '1', title: 'Programming', description: 'Programming description' },
+  { id: '2', title: 'Reading', description: 'Reading description' },
+  { id: '3', title: 'Swimming', description: 'Swimming description' },
+  { id: '4', title: 'Singing', description: 'Singing description' },
+  { id: '5', title: 'Dancing', description: 'Dancing description' }
 ];
 
 
@@ -22,11 +32,22 @@ const UserType = new GraphQLObjectType({
   name: 'User',
   description: 'Documentation for user...',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    profession: { type: GraphQLString }
   }),
 });
+
+const HobbyType = new GraphQLObjectType({
+  name: 'Hobby',
+  description: 'Hobby description...',
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString }
+  })
+})
 
 // Root query
 const RootQuery = new GraphQLObjectType({
@@ -43,6 +64,16 @@ const RootQuery = new GraphQLObjectType({
       return data.find((user) => user.id === args.id);
       },
     },
+    hobby: {
+      type: HobbyType,
+      args: { id: { type: GraphQLString } },
+
+      resolve(parent, args) {
+        // Code to get data from db / other source
+
+      return hobbiesData.find((hobby) => hobby.id === args.id);
+      },
+    }
   },
 });
 
