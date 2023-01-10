13cefda425cdf3f13cb4217847e6e5378ce220b5
XsidX <developer.sid@outlook.com>
2023-01-10 19:52:18 +0300

    Create mongoose schemas/models

---
+++ model/post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const MSchema = mongoose.Schema
+
+mongoose.set("useFindAndModify", false)
+
+const postSchema = new MSchema({
+  comment: String,
+})
+
+module.exports = mongoose.model('Post', postSchema)
