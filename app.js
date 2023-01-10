cd60ec777916a1c3ffe482bfb8687c31c62f12ed
XsidX <developer.sid@outlook.com>
2023-01-10 19:27:53 +0300

    Connect to mongo db

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const {graphqlHTTP} = require('express-graphql');
 
 const schema = require('./schema/schema');
 
+const mongoose = require('mongoose');
+
 const app = express();
+const port = process.env.PORT || 3000
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphql-cluster.8dlpxr5.mongodb.net/${process.env.mongoUserName}?retryWrites=true&w=majority`,
+ { useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  app.listen({ port }, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+ }).catch((err) => {
+  console.log(`Error::: ${err}`);
+  });
+
